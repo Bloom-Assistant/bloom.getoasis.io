aa080da58daf3a33d3073429e2d865bcc24498a8
James <james.bohrman@fermyon.com>
2024-03-21 16:57:27 -0400

    metadata trigger

--- src/jobs/updateMetadata.ts
+++ src/jobs/updateMetadata.ts
@@ -15,19 +15,20 @@ const clerk = createClerkClient({ secretKey: import.meta.env.CLERK_SECRET_KEY! }
 // This Job will be triggered by an event, log a joke to the console, and then wait 5 seconds before logging the punchline.
 client.defineJob({
     // This is the unique identifier for your Job, it must be unique across all Jobs in your project.
-    id: "allow-list",
-    name: "Allow List: Add user to allow list",
+    id: "update-metadata",
+    name: "Update Metadata: Attach Stripe metadata to user",
     version: "0.0.1",
     // This is triggered by an event using eventTrigger. You can also trigger Jobs with webhooks, on schedules, and more: https://trigger.dev/docs/documentation/concepts/triggers/introduction
-    trigger: stripe.onCheckoutSessionCompleted(),
+    trigger: stripe.onCustomerCreated(),
   run: async (payload, io, ctx) => {
       
-      await clerkClient.allowlistIdentifiers.createAllowlistIdentifier({
-        identifier: payload.customer_details?.email ?? "",
-        notify: true,
+      await clerkClient.users.updateUserMetadata(payload.id ,{
+        publicMetadata: {
+          stripeCustomerId: payload.id,
+        },
       });
       await io.logger.info(
-          "✨ Congratulations, You just ran your first successful Trigger.dev Job! ✨"
+          "✨ Congratulations, You attached the public metadata! ✨"
       );
       // To learn how to write much more complex (and probably funnier) Jobs, check out our docs: https://trigger.dev/docs/documentation/guides/create-a-job
   },
